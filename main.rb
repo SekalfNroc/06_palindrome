e1e0c362d2ef824877dc2776dafcca19be7bd48d
Sekalf Nroc <sekalf.256@gmail.com>
2017-04-19 19:13:50 +0100

    1.0.0 Found Palindrome
    Determined whether phrase is a palindrome and printed

--- main.rb
+++ main.rb
@@ -3,5 +3,19 @@
 puts "Type in a phrase."
 phrase = gets.chomp
 
-puts phrase
+class String
+  def palindrome?
+    chars.each_with_index do |character, index|
+      opposite_index = length - index - 1
+      opposite_character = self[opposite_index]
 
+      if not opposite_character == character then
+        return false
+      end
+    end
+
+    return true
+  end
+end
+
+puts phrase.palindrome?
